b11281e9975f1c2612accaccc01f490b9064ab92
Peter Dobson <peter@duckspot.com>
2014-04-07 16:39:35 -0700

    debug com.duckspot.roadie.Source

--- src/main/java/com/duckspot/roadie/Source.java
+++ src/main/java/com/duckspot/roadie/Source.java
@@ -1,36 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.duckspot.roadie;
 
+import com.duckspot.util.FileUtil;
+import com.duckspot.util.ListFile;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 /**
- *
- * @author peter
+ * Manages one source of tools.  Each source has an 'tools.list' file that 
+ * contains information about each Package available from that source, where a
+ * package is a particular version of a tool.
  */
 public class Source {
     
-    private URL url;
+    private URI uri;
     private String name;
     private String hash;
+    private Path cachePath;
+    private List<String> lines;
     
-    public Source(URL url, String name) throws MalformedURLException, IOException {
-        this.url = url;
+    public Source(URI uri, String name) throws MalformedURLException, IOException {
+        this.uri = uri;
         this.name = name;
-        hash = String.format("%08h", 0x7fffffff & url.hashCode());        
+        String temp = "0000000" 
+                + Integer.toHexString(0x7fffffff & uri.hashCode());
+        hash = temp.substring(temp.length()-8);        
+        cachePath = RoadiePaths.get("cache").resolve(hash + "_tools.list");
     }
     
-    public URL getURL() {
-        return url;
+    public URI getURI() {
+        return uri;
     }
     
     public String getName() {
@@ -38,7 +43,7 @@ public class Source {
     }
     
     public String toLine() {
-        String line = getURL().toString();
+        String line = getURI().toString();
         String name = getName();
         if (name != null && name.length() > 0) {
             line = line + " " + name;
@@ -46,14 +51,17 @@ public class Source {
         return line;
     }
     
+    public Path getCachePath() {
+        return cachePath;
+    }
+    
     /**
      * Does cache file exist?
      * 
      * @return 
      */
-    public boolean isCached() {
-        Path cacheFile = RoadiePaths.get("cache").resolve(hash);
-        return Files.exists(cacheFile);
+    public boolean isCached() {        
+        return Files.exists(cachePath);
     }
 
     /**
@@ -63,27 +71,77 @@ public class Source {
      * @throws IOException 
      */
     public boolean isOnline() throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
         connection.setRequestMethod("HEAD");
         int responseCode = connection.getResponseCode();
         return responseCode == 200;
     }
 
     /**
-     * Read tool.list from the source
+     * Read tool.list from the URL into memory.
      * 
-     * @return true if successful
+     * @return
      */
-    public boolean attemptCache() {
-        return false;
+    private void linesFromURL() throws IOException {        
+        InputStream is = uri.toURL().openStream();
+        lines = new ListFile (is).asList();
+        is.close();
     }
     
     /**
-     * Read tool.list from the source
+     * Read tool.list from the cache into memory.
+     * 
+     * @return
+     */
+    private void linesFromCache() throws IOException {        
+        lines = new ListFile (cachePath).asList();                
+    }
+    
+    /**
+     * Read tool.list from the cache into memory.
+     * 
+     * @return
+     */
+    private void linesToCache() throws IOException {
+        FileUtil.checkDirectory(cachePath.getParent());
+        Files.write(cachePath, lines, Charset.defaultCharset());
+    }
+    
+    /**
+     * Read tool.list from the source.
      * 
      * @return true if successful
      */
-    public boolean refreshCache() {
-        return false;
+    public boolean sourceToCache() {
+        try {
+            linesFromURL();            
+        } catch (IOException ex) {
+            // this can happen w. server is unreachable or down
+//            return false;
+            throw new Error("test exception", ex);
+        }
+        try {
+            linesToCache();
+        } catch (IOException ex) {
+            // TODO: consider disk full
+            // TODO: display useful message to user
+            System.out.println("unexpected exception: "+ex);
+            throw new Error("unexpected exception", ex);
+        }
+        return true;
+    }
+    
+    public List<String> getLines() {
+        if (lines == null && !isCached()) {
+            sourceToCache();
+        }
+        if (lines == null) {
+            try {
+                linesFromCache();
+            } catch (IOException ex) {
+                return null;
+            }
+        }
+        return lines;
     }
 }
---
+++ src/test/java/com/duckspot/roadie/SourceTest.java
@@ -0,0 +1,172 @@
+package com.duckspot.roadie;
+
+import com.duckspot.util.FileUtil;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SourceTest {
+    
+    static URI sourceURI;
+    static String name;
+    
+    public SourceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() throws URISyntaxException {
+        sourceURI = new URI("http://duckspot.com/roadie-tools/package.list");
+        name = "duckspot.com/roadie-tools";
+        RoadiePaths.setDevRoot("dev");
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() throws IOException {
+        FileUtil.deleteDirectory(Paths.get("dev"));
+    }
+    
+    @After
+    public void tearDown() {        
+    }
+
+    @Test
+    public void testAssumptions() throws MalformedURLException, IOException {
+        int number = 0x7fff;
+        String expResult = "00007fff";
+        String temp = "0000000" + Integer.toHexString(number);
+        String result = temp.substring(temp.length()-8);
+        assertEquals(expResult, result);
+        number = 0;
+        expResult = "00000000";
+        temp = "0000000" + Integer.toHexString(number);
+        result = temp.substring(temp.length()-8);
+        assertEquals(expResult, result);
+        number = 0x7fffffff;
+        expResult = "7fffffff";
+        temp = "0000000" + Integer.toHexString(number);
+        result = temp.substring(temp.length()-8);
+        assertEquals(expResult, result);
+        URL sourceURL = sourceURI.toURL();
+        InputStream is = sourceURL.openStream();        
+        is.close();
+    }
+    /**
+     * Test of getURI method, of class Source.
+     */
+    @Test
+    public void testGetURI() throws IOException {
+        System.out.println("getURI");        
+        Source instance = new Source(sourceURI, name);
+        URI expResult = sourceURI;
+        URI result = instance.getURI();
+        assertEquals(expResult, result);        
+    }
+
+    /**
+     * Test of getName method, of class Source.
+     */
+    @Test
+    public void testGetName() throws IOException {
+        System.out.println("getName");
+        Source instance = new Source(sourceURI, name);
+        String expResult = name;
+        String result = instance.getName();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of toLine method, of class Source.
+     */
+    @Test
+    public void testToLine() throws IOException {
+        System.out.println("toLine");
+        Source instance = new Source(sourceURI, name);
+        String expResult = sourceURI+" "+name;
+        String result = instance.toLine();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testGetCachePath() throws IOException {
+        System.out.println("isCached");
+        Source instance = new Source(sourceURI, name);        
+        Path expResult = RoadiePaths.get("cache").resolve("5988b905_tools.list");
+        Path result = instance.getCachePath();
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of isCached method, of class Source.
+     */
+    @Test
+    public void testIsCached() throws IOException {
+        System.out.println("isCached");
+        Source instance = new Source(sourceURI, name);
+        boolean expResult = false;
+        boolean result = instance.isCached();
+        assertEquals(expResult, result);
+        instance.sourceToCache();
+        expResult = true;
+        result = instance.isCached();
+        assertEquals(expResult, result);        
+    }
+
+    /**
+     * Test of isOnline method, of class Source.
+     */
+    @Test
+    public void testIsOnline() throws IOException {
+        System.out.println("isOnline");
+        Source instance = new Source(sourceURI, name);
+        boolean expResult = true;
+        boolean result = instance.isOnline();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of SourceToCache method, of class Source.
+     */
+    @Test
+    public void testSourceToCache() throws IOException {
+        System.out.println("SourceToCache");        
+        Source instance = new Source(sourceURI, name);
+        assertFalse(instance.isCached());
+        boolean expResult = true;
+        boolean result = instance.sourceToCache();
+        assertEquals(expResult, result);        
+        assertTrue(instance.isCached());
+    }
+
+    /**
+     * Test of getLines method, of class Source.
+     */
+    @Test
+    public void testGetLines() throws IOException {
+        System.out.println("getLines");
+        System.out.println("sourceURI:"+sourceURI);
+        Source instance = new Source(sourceURI, name);
+        String[] lines = {"git 1.8.5.2.msysgit.0 "
+                + "git/git_1.8.5.2.msysgit.0_install.json"};
+        List<String> expResult = Arrays.asList(lines);
+        List<String> result = instance.getLines();
+        assertEquals(expResult, result);        
+    }
+    
+}
